fe65ad3a1ebbf413078b23d6db47f5a0febdb9f8
Bence A. Toth <tothab@gmail.com>
2024-04-22 00:09:01 +0200

    Added some sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { AiFillFacebook as FacebookIcon } from "react-icons/ai";
 import { AiFillInstagram as InstragramIcon } from "react-icons/ai";
 
-import { ticketPurchaseLink, facebookLink, instagramLink } from "./constants";
+import {
+  ticketPurchaseLink,
+  facebookLink,
+  instagramLink,
+  programme,
+} from "./constants";
 
 import "./App.css";
 
 import Logo from "./logo.png";
+import Banner from "./banner.jpg";
 
 const App = () => (
   <div>
@@ -55,6 +61,76 @@ const App = () => (
         </nav>
       </div>
     </header>
+    <main>
+      <section className="banner">
+        <img src={Banner} alt="5. Boglart, 2024, Balatonboglár, Babel Camp" />
+      </section>
+      <section>
+        <h2>Programok</h2>
+        <table className="desktop-schedule" border={1}>
+          <thead>
+            <td></td>
+            {programme.map((daySchedule) => (
+              <th>{daySchedule.day}</th>
+            ))}
+          </thead>
+          <tbody>
+            {programme[0].schedule.map((timeSlot, timeSlotIndex) => (
+              <tr>
+                <th>{timeSlot.time}</th>
+                {programme.map((daySchedule) => (
+                  <th>
+                    <p>{daySchedule.schedule[timeSlotIndex].name}</p>
+                    <p>{daySchedule.schedule[timeSlotIndex].location}</p>
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <br />
+        <table className="mobile-schedule" border={1}>
+          <tbody>
+            {programme.map((daySchedule) => (
+              <>
+                <tr>
+                  <td></td>
+                  <th>{daySchedule.day}</th>
+                </tr>
+                {daySchedule.schedule.map((timeSlot) => (
+                  <tr>
+                    <th>{timeSlot.time}</th>
+                    <td>
+                      <p>{timeSlot.name}</p>
+                      <p>{timeSlot.location}</p>
+                    </td>
+                  </tr>
+                ))}
+              </>
+            ))}
+          </tbody>
+        </table>
+      </section>
+      <section>
+        <h2>További programok</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae
+          placeat quas ea! Quaerat quas ipsum recusandae cum repellat,
+          exercitationem voluptatibus ullam labore architecto necessitatibus
+          possimus eaque impedit in iure. Sint. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. At libero incidunt illum et cumque
+          laborum quae pariatur blanditiis voluptate, maiores itaque facilis eos
+          vel quibusdam laboriosam ratione ducimus esse ea. Lorem ipsum dolor
+          sit amet, consectetur adipisicing elit. Velit quam a vero blanditiis,
+          aliquam ex esse! Velit itaque vitae atque qui eum, et quaerat quod
+          beatae magnam repellendus alias quas. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Vel voluptate soluta nobis? Eligendi
+          quibusdam modi at optio illum, sunt ad dicta. Tenetur nulla quod ab
+          tempora atque libero nisi laboriosam?
+        </p>
+      </section>
+      <footer></footer>
+    </main>
   </div>
 );
 
--- src/constants.tsx
+++ src/constants.tsx
@@ -1,6 +1,131 @@
+/* eslint-disable sonarjs/no-duplicate-string */
+
 export const ticketPurchaseLink =
   "https://tixa.hu/V_Boglart_Fesztival_20240828_Balatonboglar";
 
 export const instagramLink = "https://www.instagram.com/boglart.feszt/";
 
 export const facebookLink = "https://www.facebook.com/boglartfeszt";
+
+export const programme = [
+  {
+    day: "2024-10-28",
+    schedule: [
+      {
+        time: "19:00",
+        name: "Meybahar [HU/GR]",
+        location: "Nagyszínpad",
+      },
+      {
+        time: "20:00",
+        name: "Obadu Live Act",
+        location: "Civil falu",
+      },
+      {
+        time: "20:30",
+        name: "Nogaband [SK]",
+        location: "Nagyszínpad",
+      },
+      {
+        time: "22:00",
+        name: "Tindia",
+        location: "Romkocsma",
+      },
+      {
+        time: "23:00",
+        name: "Vonós és moldvai táncház",
+        location: "Romkocsma",
+      },
+    ],
+  },
+  {
+    day: "2024-10-29",
+    schedule: [
+      {
+        time: "19:00",
+        name: "Boyongó",
+        location: "Nagyszínpad",
+      },
+      {
+        time: "20:00",
+        name: "Obadu Live Act",
+        location: "Civil falu",
+      },
+      {
+        time: "20:30",
+        name: "Ferenczi György és az 1ső Pesti Rackák",
+        location: "Nagyszínpad",
+      },
+      {
+        time: "22:00",
+        name: "Kapcza Banda",
+        location: "Romkocsma",
+      },
+      {
+        time: "23:00",
+        name: "Vonós és moldvai táncház",
+        location: "Romkocsma",
+      },
+    ],
+  },
+  {
+    day: "2024-10-30",
+    schedule: [
+      {
+        time: "19:00",
+        name: "Times New Román",
+        location: "Nagyszínpad",
+      },
+      {
+        time: "20:00",
+        name: "Obadu Live Act",
+        location: "Civil falu",
+      },
+      {
+        time: "20:30",
+        name: "Poklade",
+        location: "Nagyszínpad",
+      },
+      {
+        time: "22:00",
+        name: "Pásztorhóra",
+        location: "Romkocsma",
+      },
+      {
+        time: "23:00",
+        name: "Vonós, moldvai és nemzetiségi táncház",
+        location: "Romkocsma",
+      },
+    ],
+  },
+  {
+    day: "2024-10-31",
+    schedule: [
+      {
+        time: "19:00",
+        name: "Alterne Trio (BE/EE/DK)",
+        location: "Nagyszínpad",
+      },
+      {
+        time: "20:00",
+        name: "Obadu Live Act",
+        location: "Civil falu",
+      },
+      {
+        time: "20:30",
+        name: "Góbé",
+        location: "Nagyszínpad",
+      },
+      {
+        time: "22:00",
+        name: "Zajnal",
+        location: "Romkocsma",
+      },
+      {
+        time: "23:00",
+        name: "Vonós és moldvai táncház",
+        location: "Romkocsma",
+      },
+    ],
+  },
+];
