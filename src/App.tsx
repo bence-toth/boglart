f3e049b33234e18cc8879d76d80239ab77588814
Bence A. Toth <tothab@gmail.com>
2024-04-21 23:03:44 +0200

    Some basic stuff, header

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,38 @@
-const App = () => <div></div>;
+import { ticketPurchaseLink } from "./constants";
+
+import "./App.css";
+
+import Logo from "./logo.png";
+
+const App = () => (
+  <div>
+    <header className="header">
+      <div className="header-inner-wrapper">
+        <div className="logo-wrapper">
+          <img className="header-logo" src={Logo} alt="Boglart" />
+        </div>
+        <nav className="header-navigation">
+          <ul>
+            <li>
+              <a href={ticketPurchaseLink} target="_blank" rel="noreferrer">
+                English
+              </a>
+            </li>
+            <li>
+              <a
+                className="buy-ticket"
+                href={ticketPurchaseLink}
+                target="_blank"
+                rel="noreferrer"
+              >
+                Jegyvásárlás
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  </div>
+);
 
 export default App;
---
+++ src/constants.tsx
@@ -0,0 +1,2 @@
+export const ticketPurchaseLink =
+  "https://tixa.hu/V_Boglart_Fesztival_20240828_Balatonboglar";
